d85eb536e694c172517bd7b3702e89392fdc7332
selinbagdaa <selinbada@gmail.com>
2024-04-25 12:43:23 +0300

    selin2

--- src/Ortak.java
+++ src/Ortak.java
@@ -2,7 +2,7 @@ public class Ortak {
     public static void main(String[] args) {
 
         System.out.println("Ne yaparsan yap AŞK ile yap");
-
+        System.out.println("Merhaba Selin mainde Push Etti az önce");
 
 
 
