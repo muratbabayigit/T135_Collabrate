179f95813f60f41249e3e8ddb6e98937b1dc136a
Elif Kesen <elifkesen@gmail.com>
2024-04-25 10:33:48 +0200

    elif 24.04.2024

--- src/Ortak.java
+++ src/Ortak.java
@@ -2,6 +2,7 @@ public class Ortak {
     public static void main(String[] args) {
 
         System.out.println("Ne yaparsan yap AŞK ile yap");
+        System.out.println("Elif´ten dünyaya merhaba!");
 
 
 
