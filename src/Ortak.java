dc251753072e4406be8ed41ba65bfc742403be72
babayigit <babayigit@gmail.com>
2024-04-25 11:29:14 +0300

    babayigit 25.04.2024 v1.0.1

--- src/Ortak.java
+++ src/Ortak.java
@@ -2,7 +2,7 @@ public class Ortak {
     public static void main(String[] args) {
 
         System.out.println("Ne yaparsan yap AŞK ile yap");
-
+        System.out.println("Ortalık karışacak vaziyet alın");
 
 
 
