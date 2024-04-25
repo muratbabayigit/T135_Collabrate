fcf8553b2f87401bfbee50dc84d814c2682641b8
babayigit <babayigit@gmail.com>
2024-04-25 12:23:57 +0300

    babayigit 25042024 v1.1

--- src/Ortak.java
+++ src/Ortak.java
@@ -2,11 +2,8 @@ public class Ortak {
     public static void main(String[] args) {
 
         System.out.println("Ne yaparsan yap AŞK ile yap");
-        System.out.println("Elif´ten dünyaya merhaba!");
-        System.out.println("Ortalık karışacak vaziyet alın");
-        System.out.println("II.Merge Harbi");
-        System.out.println("deneme selçuk");
-        System.out.println("Hersey cok guzel olacak");
+
+
 
 
         
